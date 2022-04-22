60117a7742479574d51ea994d6a9a76bdf74e338
luke crawford <lukecrawford@lukes-Mac-mini.local>
2022-04-21 21:13:36 -0700

    added quadratic homework

---
+++ disha/quadratics/bisection.py
@@ -0,0 +1,23 @@
+#!/usr/local/bin/python3
+import sys, getopt
+
+
+def quadraticforX(x):
+  print ("when X is %f, y is %f" % (x, (x*x)-(45*x)+324) )
+  return (x*x)-(45*x)+324
+
+highx=float(10)
+lowx=float(5)
+y=5 
+while (abs(y) > float(0.00001)):
+  x=(highx+lowx)/2
+  print ("x is %f" % x)
+  y=quadraticforX(x)
+  if y < 0:
+    highx=x
+  else:
+    lowx=x
+  
+
+
+  
